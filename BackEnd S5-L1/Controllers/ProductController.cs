60062a2ac6df21ba3aaa56c2206d2a863289404c
MREBECCA98 <reb.matarozzo@gmail.com>
2025-12-02 16:45:46 +0100

    modify services (add create and gett all)

---
+++ BackEnd S5-L1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using BackEnd_S5_L1.Models.Entity;
+using BackEnd_S5_L1.Models.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_S5_L1.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _productService.GetAll();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            product.Id = Guid.NewGuid();
+            bool isCreated = _productService.Create(product);
+
+
+            return View(product);
+
+        }
+    }
+}
